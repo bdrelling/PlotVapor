233d7c53df803217927c5387b1eb018ddf0ec6ee
Brian Drelling <brian@drelling.com>
2022-07-30 14:15:05 -0500

    Updated to Plot 0.11.0, improved Vapor backwards compatibility

--- Package.swift
+++ Package.swift
@@ -5,14 +5,14 @@ import PackageDescription
 let package = Package(
     name: "PlotVapor",
     platforms: [
-        .macOS(.v10_15),
+        .macOS(.v10_15),    // required for Vapor
     ],
     products: [
         .library(name: "PlotVapor", targets: ["PlotVapor"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/JohnSundell/Plot", .upToNextMinor(from: "0.10.0")),
-        .package(url: "https://github.com/vapor/vapor", .upToNextMajor(from: "4.54.0")),
+        .package(url: "https://github.com/JohnSundell/Plot", .upToNextMinor(from: "0.11.0")),
+        .package(url: "https://github.com/vapor/vapor", from: "4.0.0"),
     ],
     targets: [
         .target(
