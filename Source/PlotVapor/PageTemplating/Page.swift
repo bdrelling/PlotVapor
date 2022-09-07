119e314310af9c072f04d0c2dfcd4e9544bf6649
Brian Drelling <brian@drelling.com>
2022-09-07 00:40:04 -0500

    Added @ComponentBuilder to Page

--- Source/PlotVapor/PageTemplating/Page.swift
+++ Source/PlotVapor/PageTemplating/Page.swift
@@ -5,10 +5,11 @@ import Plot
 public protocol Page: Renderable {
     var title: String { get }
     var themeColor: String? { get }
-    
+
     var head: Node<HTML.DocumentContext> { get }
     var body: Node<HTML.DocumentContext> { get }
-    var content: Component { get }
+
+    @ComponentBuilder var content: Component { get }
 
     func toHTML() -> HTML
 }
@@ -33,25 +34,25 @@ public extension Page {
             return .head(.title(self.title))
         }
     }
-    
+
     var body: Node<HTML.DocumentContext> {
         .body(
             .component(self.content)
         )
     }
-    
+
     func toHTML() -> HTML {
         .init(
-            head,
-            body
+            self.head,
+            self.body
         )
     }
 }
 
 // MARK: Renderable
 
-extension Page {
-    public func render(indentedBy indentationKind: Indentation.Kind?) -> String {
+public extension Page {
+    func render(indentedBy indentationKind: Indentation.Kind?) -> String {
         self.toHTML().render(indentedBy: indentationKind)
     }
 }
